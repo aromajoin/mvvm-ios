0b208876a47486fd90eb8cfdb470d6d01e1e1c91
Behnam <b.rakhshani77@gmail.com>
2022-11-08 09:32:07 +0330

    Implement Network Layer

---
+++ mvvm-ios/Resource.swift
@@ -0,0 +1,13 @@
+//
+//  Resource.swift
+//  mvvm-ios
+//
+//  Created by Behnam on 11/7/22.
+//  Copyright © 2022 Aromajoin. All rights reserved.
+//
+
+import Foundation
+
+struct Resource<T: Decodable> {
+    let endpoint: Endpoint
+}
